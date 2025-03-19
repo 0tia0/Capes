bb87ed949542e98e8d9dcb5f8c0ce4f27397ba9b
Mattia <162014917+0tia0@users.noreply.github.com>
2025-03-19 19:44:29 +0100

    first commit

---
+++ src/client/java/me/mattia/capes/client/CapesClient.java
@@ -0,0 +1,10 @@
+package me.mattia.capes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CapesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/mattia/capes/Capes.java
@@ -0,0 +1,10 @@
+package me.mattia.capes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Capes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
